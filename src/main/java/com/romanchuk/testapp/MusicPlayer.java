4f03b8252fa779009d8c676b791689399508c4d1
vazonchik <romanchuk552@gmail.com>
2021-03-22 18:04:19 +0200

    Dependency Injection - @Qualifier

--- src/main/java/com/romanchuk/testapp/MusicPlayer.java
+++ src/main/java/com/romanchuk/testapp/MusicPlayer.java
@@ -1,22 +1,24 @@
 package com.romanchuk.testapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
+
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1,@Qualifier("rockMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
 
     }
 }
