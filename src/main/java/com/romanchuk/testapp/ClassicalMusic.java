aeb999f029fd3be1a942edfbbb958430ad6bc363
vazonchik <romanchuk552@gmail.com>
2021-03-22 15:09:33 +0200

    Bean Lifecycle

--- src/main/java/com/romanchuk/testapp/ClassicalMusic.java
+++ src/main/java/com/romanchuk/testapp/ClassicalMusic.java
@@ -1,6 +1,19 @@
 package com.romanchuk.testapp;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic(){};
+
+    public static ClassicalMusic classicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
 
     @Override
     public String getSong() {
--- src/main/java/com/romanchuk/testapp/TestSpring.java
+++ src/main/java/com/romanchuk/testapp/TestSpring.java
@@ -6,12 +6,14 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean",ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusicList();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
